4aa57c0668711e3608effa4d3d74109a063dfca6
624605062 <624605062@qq.com>
2021-01-07 10:25:13 +0800

    第二次提交

--- day001.py
+++ day001.py
@@ -75,4 +75,57 @@ print ('\n python 路径为',sys.path)'''
 age1=int(age)
 print('我的年龄是：%d'%age1)#%r可以代表任意数据类型 %s 字符串 %d数字
 '''
+'''def reverseWords(input):
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = input.split(" ")
 
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords = inputWords[-1::-1]
+
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+
+    return output
+
+
+if __name__ == "__main__":
+    input = 'I like runoob'
+    rw = reverseWords(input)
+    print(rw)'''
+
+'''sites = {'Google', 'Taobao', 'Runoob', 'Facebook', 'Zhihu', 'Baidu',}
+
+print(sites)   # 输出集合，重复的元素被自动去掉
+if 'Runoob' in sites :
+    print('Runoob 在集合中')
+else :
+    print('Runoob 不在集合中')
+a = set('abracadabra')
+b = set('alacazam')
+
+print(a)
+
+print(a - b)  # a 和 b 的差集
+
+print(a | b)  # a 和 b 的并集
+print(a & b)  # a 和 b 的交集
+print(a ^ b)  # a 和 b 中不同时存在的元素'''
+'''def test(*args):
+    print(args)
+    return args
+
+print(type(test(1,2,3,4)))'''
+
+'''def example(d):
+    dict[d] = {1, 2}
+    # d 是一个字典对象
+    for c in d:
+        print(c)
+        #如果调用函数试试的话，会发现函数会将d的所有键打印出来;
+        #也就是遍历的是d的键，而不是值.'''
