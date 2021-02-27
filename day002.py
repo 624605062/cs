74dcdca84498df4dba5fe734d80e160259f9298e
624605062 <624605062@qq.com>
2021-02-27 17:23:57 +0800

    2021/2/22

--- day002.py
+++ day002.py
@@ -29,7 +29,7 @@
         print("所有问卷为：")
         for response in self.responses:
             print("-"+response)'''
-class AnonymousSurvey():
+'''class AnonymousSurvey():
     """收集匿名调查问卷的答案"""
     def __init__(self, question):
         """存储一个问题，并为存储答案做准备"""
@@ -45,4 +45,24 @@ class AnonymousSurvey():
         """显示收集到的所有答卷"""
         print("Survey results:")
     for response in self.responses:
-        print('- ' + response)
+        print('- ' + response)'''
+
+'''class Employee():
+    def __init__(self,firstname,lastname,job=0):
+        self.firstname=firstname
+        self.lastname=lastname
+        self.job=job
+    def give_raise(self,salary=5000):
+        self.job+=salary
+        return salary
+import unittest
+class TestEmployee(unittest.TestCase):
+    def setUp(self) -> None:
+        self.longwj=Employee('long','wj',5000)
+    def test_give_default_raise(self):
+        names=self.longwj.give_raise()
+        self.assertEqual(names,5000)
+    def test_give_custom_raise(self):
+        ournames=self.longwj.give_raise(6000)
+        self.assertEqual(ournames,16000)
+unittest.main()'''
--- 自动注册用户.py
+++ 自动注册用户.py
@@ -2,19 +2,15 @@ from selenium import webdriver
 import time
 from time import  sleep
 import unittest
-from selenium.webdriver.common.action_chains import ActionChains
-#登录系统
-driver = webdriver.Chrome() #打开浏览器
-driver.get("http://171.34.197.243:8081/ZHHB/page/login.html") #访问url
- #最大化窗口
-driver.maximize_window()
-sleep(3)
-###登录用户
-driver.find_element_by_xpath('/html/body/div/div[5]/div[1]/div/input').send_keys('super')
-driver.find_element_by_id('password').send_keys('11111111')
-driver.find_element_by_class_name('logbtn').click()
-##等待10秒 手动输入验证
-######进入污染源系统
-sleep(3)
-driver.get('http://171.34.197.243:8081/wryzxjc/dist/index.html#/')
+username='superstart'
+passwd='yshb@123..'
+url='http://171.34.197.243:8081/ZHHB/page/login.html'
+driver=webdriver.chrome
+
+
+
+class Test_yshb(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver=webdriver.chrome
+
 
